1ce3ceac3b2ac20d8924477fa7623a0a71cd0665
Wojciech <wojt648@gmail.com>
2025-04-03 19:08:43 +0200

    First Commit

---
+++ cv-polisher/src/main/java/com/cvpolisher/App.java
@@ -0,0 +1,13 @@
+package com.cvpolisher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
