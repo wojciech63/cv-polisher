84cf15b4c70cdce371a19cfa00d797df75b221d8
Wojciech <wojt648@gmail.com>
2025-04-16 17:31:44 +0200

    empty constructor for certification

--- desktopmvp/desktop/src/main/java/com/desktopmvp/model/Certification.java
+++ desktopmvp/desktop/src/main/java/com/desktopmvp/model/Certification.java
@@ -1,14 +1,29 @@
 package com.desktopmvp.model;
 
+import javax.annotation.processing.Generated;
+
+@Entity
+@Table (name = "Certification")
 public class Certification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT")
+    private long id;
+    @Column(name = "certificate_name", columnDefinition = "VARCHAR(255)")
     private String certificateName;
+    @Column(name = "issuer", columnDefinition = "VARCHAR(255)")
     private String issuedBy;
+    @Column(name = "date", columnDefinition = "DATE")
     private java.time.YearMonth date;
+    @Column(name = "description", columnDefinition = )
     private String description;
     
     public Certification(String certificateName, String issuedBy) {
         this.certificateName = certificateName;
         this.issuedBy = issuedBy;
+    }
+
+    public Certification() {
     }
 
     public String getCertificateName() {
